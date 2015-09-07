c4fdd889b64a53d5c1b336eca8c7fcf2fcbad201
Cody Antcliffe <codyantcliffe@gmail.com>
2015-09-07 14:33:51 -0400

    2.1 cleanup

--- Chapter 2/src/duplicateDelete.java
+++ Chapter 2/src/duplicateDelete.java
@@ -1,10 +1,7 @@
 /* 
  * 2.1
  * Write code to remove duplicates from an unsorted linked-list.
- * FOLLOW UP:
- * How would you solve this problem if a temporary buffer is not allowed? 
  */
-
 import java.util.*;
 
 public class duplicateDelete {
